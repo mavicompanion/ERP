78ef17a65b478c09ddc6dd6419d629c7cdfbfc70
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2024-04-05 17:39:38 +0530

    Added Employee registration module.
    Created Employee registration module where new user will be created by fetching admin's enterprise domain and enterprise. You can list and Add Employee.

--- src/main/java/com/ERP/v1/controller/AdminController.java
+++ src/main/java/com/ERP/v1/controller/AdminController.java
@@ -5,15 +5,22 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ERP.v1.dto.EmployeeDto;
+import com.ERP.v1.model.Enterprise;
 import com.ERP.v1.service.EmployeeService;
+import com.ERP.v1.service.EnterpriseService;
 
 @Controller
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
+
+    @Autowired
+    EnterpriseService enterpriseService;
 
     @Autowired
     EmployeeService employeeService;
@@ -29,13 +36,24 @@ public class AdminController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/admin/employeeRegistration")
-    public ModelAndView userRegistration(@AuthenticationPrincipal UserDetails userDetails)
+    public ModelAndView userRegistration(@AuthenticationPrincipal UserDetails userDetails, EmployeeDto employeeDto)
     {
         ModelAndView modelAndView = new ModelAndView("employeeRegistration");
         String userName = userDetails.getUsername();
         modelAndView.addObject("userName", userName);
         modelAndView.addObject("employees", employeeService.getAllEmployeesByDomain(employeeService.getDomainByEmail(userDetails.getUsername())));
+        modelAndView.addObject("newUser", employeeDto);
         return modelAndView;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/employeeRegistration")
+    public String registerEmployee(@AuthenticationPrincipal UserDetails userDetails, @ModelAttribute EmployeeDto employeeDto)
+    {
+        Enterprise enterprise = enterpriseService.getEnterprise(
+            employeeService.getDomainByEmail(userDetails.getUsername())
+            );
+        employeeService.createEmployee(employeeDto, enterprise);
+        return "redirect:/admin/employeeRegistration";
     }
 
 }
