aabf78b2f17e413068c44cfbef14feedbbc567ff
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2024-04-12 13:49:43 +0530

    Changes with unique employees column, and removed unnecessary modals and dto's

--- src/main/java/com/ERP/v1/repository/CategoryPropertyRepositor.java
+++
@@ -1,9 +0,0 @@
-package com.ERP.v1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ERP.v1.model.CatergoryProperty;
-
-public interface CategoryPropertyRepositor extends JpaRepository<CatergoryProperty, Long> {
-    
-}
